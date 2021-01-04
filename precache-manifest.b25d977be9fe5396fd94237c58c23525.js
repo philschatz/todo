c4c85048751b86eb52e7b6e712e0bf491d14ac2b
philschatz <253202+philschatz@users.noreply.github.com>
2021-01-03 22:24:42 -0600

    add built files

---
+++ precache-manifest.b25d977be9fe5396fd94237c58c23525.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9699ed6ed22579d08a81e6c448498587",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "947a132372e2a5b0db10",
+    "url": "/todo/static/js/2.eedd0bfc.chunk.js"
+  },
+  {
+    "revision": "f80f51859a953903f9181f04efa9bc15",
+    "url": "/todo/static/js/2.eedd0bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "992e3ca83491aaab9bc0",
+    "url": "/todo/static/js/main.0f402d78.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
